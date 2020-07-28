ce8564bbae3d8cd478cefc24dbca8775b28e7c9d
Harold F <harold.f415@gmail.com>
2020-07-27 21:18:38 -0400

    challenge-completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 
+import Login from "./Login";
+
+var isLoggedIn = true;
+
+const currentTime = new Date(2020, 7, 4, 18).getHours(); // setting the time past 12 so h1 will render
+
 function App() {
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
+      {// Ternary Operator
+      // isLoggedIn ? <h1>Hello</h1> : <Login />
+
+      // AND Operator
+      currentTime > 12 && <h1>Why you working late?</h1>}
     </div>
   );
 }
